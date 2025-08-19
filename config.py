9c980fc1ce32eae60ad04864dd9f8a2e171fcd96
jepbarturkmen <jepbarturkmen@gmail.com>
2025-08-19 15:50:49 +0500

    config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 
 # Atakhan tarafından sağlanan gerçek bilgiler
-TOKEN = "7969003527:AAEaOwFnoC7O4dxoRCooA8Lvz94HWntPIc4"
+TOKEN = "7969003527:AAH9g05WO6AMWRawHtUGInDAt-tPN-UNmys"
 ADMIN_USER_ID = 8067876866
